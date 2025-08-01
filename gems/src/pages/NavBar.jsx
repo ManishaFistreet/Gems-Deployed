c81c7a3bbfcd39cefdf24674c50ea04a5a07a910
ManishaFistreet <manisha@fistreet.in>
2025-08-01 09:29:33 +0530

    logout button

--- gems/src/pages/NavBar.jsx
+++ gems/src/pages/NavBar.jsx
@@ -1,8 +1,16 @@
 // src/components/Navbar.js
 import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function Navbar() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    navigate('/login');
+  };
+
   return (
     <AppBar position="static" color="primary">
       <Toolbar>
@@ -14,6 +22,7 @@ function Navbar() {
           <Button color="inherit" component={Link} to="/fill">Fill</Button>
           <Button color="inherit" component={Link} to="/print">Print</Button>
           <Button color="inherit" component={Link} to="/purchase_list">Purchase List</Button>
+          <Button color="inherit" onClick={handleLogout}>Logout</Button>
         </Box>
       </Toolbar>
     </AppBar>
