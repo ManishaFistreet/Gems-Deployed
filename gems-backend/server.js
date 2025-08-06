a3330314e1217e762108eb710c850a525126a089
ManishaFistreet <manisha@fistreet.in>
2025-08-06 12:15:47 +0530

    fix for space fo payload

--- gems-backend/server.js
+++ gems-backend/server.js
@@ -18,7 +18,8 @@ app.use(cors({
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
 }));
 
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
